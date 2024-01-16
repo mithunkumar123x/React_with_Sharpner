4ff9113c337c0fddce51eb0d8e1267d705c1a042
mithunkumar123x <mithunkumar123x@gmail.com>
2024-01-16 12:53:42 +0530

    add some expenses underneath the Expense Item Header.

---
+++ 01reactbasics/src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+function ExpenseItem() {
+    return  (
+        <div>
+            <div>
+                <h3>Food Items Rs 10</h3>
+            </div>
+            <div>
+                <h3>Petrol Rs 100</h3>
+            </div>
+            <div>
+                <h3>Movies Rs 200</h3>
+            </div>
+        </div>
+       
+    )
+         
+          
+}
+
+export default ExpenseItem;
