85246c56644356406455f039d1cd0feb7ffe9d18
mithunkumar123x <mithunkumar123x@gmail.com>
2024-01-19 10:00:15 +0530

    Add function to use write 100 component .

--- 01reactbasics/src/App.js
+++ 01reactbasics/src/App.js
@@ -1,9 +1,71 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from './components/ExpenseItem';
 
-function App() {
+
+const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: '  Chopstick',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      localExpenditure:"Pune"
+    },
+    { id: 'e2', 
+    title: 'New TV',
+     amount: 799.49,
+      date: new Date(2021, 2, 12),
+      localExpenditure:"Delhi" },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      localExpenditure:"Mumbai"
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      localExpenditure:"Banglore"
+    },
+    {
+      id: 'e5',
+      title: 'glass',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      localExpenditure:"Pune"
+    },
+    { id: 'e6', 
+    title: 'New TV', 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12),
+    localExpenditure:"Delhi" },
+    {
+      id: 'e7',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      localExpenditure:"Mumbai"
+    },
+    {
+      id: 'e8',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      localExpenditure:"Banglore"
+    },
+  ];
   return (
-    <div>
-      <ExpenseItem></ExpenseItem>    
+    <div className="App">
+      {expenses.map((expense,index)=>(
+        <ExpenseItem 
+        key = {index}
+        title = {expense.title}
+        date ={expense.date}
+        amount={expense.amount}
+        localExpenditure={expense.localExpenditure} />
+      ))}
     </div>
   );
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # React_with_Sharpner
 start learning with Sharpner
+
+
+you have 100 expenses you would have to write ExpenseItems Component 100 times. 
+
+use of .map to write js in html tag.
