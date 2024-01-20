ccb6537bd69c467a762eb5fa088aa4cc9cd17fb8
mithunkumar123x <mithunkumar123x@gmail.com>
2024-01-20 22:50:50 +0530

    optional refrence change in app.js

--- 01reactbasics/src/components/ExpenseItem.js
+++ 01reactbasics/src/components/ExpenseItem.js
@@ -3,21 +3,24 @@ import './ExpenseItem.css';
 import Card from './Card' ;
 
 
-function ExpenseItem(props) {
+function ExpenseItem(props){
 
 
     const LocationOfExpenditure = ["Resturant","Fuel" ,"Cinema hall" ]; 
-
+     console.log(props);
     return ( 
         <Card className="expense-item">
-      {LocationOfExpenditure.map((location, index) => (
-        <div key={index} className="expense-item_description">
-          <ExpenseDate date={props.date} />
+
+     
+          
+        <div className="expense-item_description">
+       
           <h2>{props.title}</h2>
           <div className="expense-item-price">${props.amount}</div>
-          <div>Location: {location}</div>
+          <div className = ""> </div>
+          <ExpenseDate date={props.date} />
         </div>
-      ))}
+      
     </Card>
   );
 }
